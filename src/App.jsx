0cc1d48b7cedcd6d12a0a0646fa03a288d685233
Felipe de Senna <lipe.senna@hotmail.com>
2023-08-24 19:02:02 +0100

    fix select numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,31 @@ import { useEffect, useState, useId } from 'react'
 
 import './App.css'
 
+const initialConfiguration = [1, 2, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
 function App() {
   const zeroId = useId()
-  const initialConfiguration = [1, 2, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  const [newValue, setNewValue] = useState(0)
-  const [newState, setNewState] = useState(initialConfiguration)
+  const [puzzleId, setPuzzleId] = useState(0)
+  const [newState, setNewState] = useState([])
+
+  function onChangePuzzle(value) {
+    const findIndex = initialConfiguration.indexOf(value)
+    const findIndexZero = initialConfiguration.indexOf(0)
+
+    initialConfiguration.splice(findIndex, 1, 0)
+    initialConfiguration.splice(findIndexZero, 1, value)
+
+    return initialConfiguration
+  }
 
   useEffect(() => {
-    if (newValue !== 0) {
-      const findIndex = initialConfiguration.indexOf(newValue)
-      const findIndexZero = initialConfiguration.indexOf(0)
-
-      initialConfiguration.splice(findIndex, 1, 0)
-      initialConfiguration.splice(findIndexZero, 1, newValue)
-
+    if (puzzleId !== 0) {
+      const result = onChangePuzzle(puzzleId)
+      setNewState([...result])
+    } else {
       setNewState(initialConfiguration)
     }
-  }, [newValue])
+  }, [puzzleId])
 
   function onSolveCallback() {
     alert('Sucesso ao resolver o puzzle')
@@ -29,7 +37,7 @@ function App() {
       {newState.map(value => value == 0 ? (
         <div key={zeroId} className="empty" onClick={onSolveCallback} />
       ) : (
-        <div key={value} className="tile" onClick={() => setNewValue(value)}>{value}</div>
+        <div key={value} className="tile" onClick={() => setPuzzleId(value)}>{value}</div>
       ))}
     </div>
   )
